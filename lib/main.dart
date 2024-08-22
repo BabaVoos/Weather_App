67d9c0ca1df954a48737b3b2b609862edf9f25eb
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-08-22 07:24:05 +0300

    🛤️ Added App router and routes class 🏠 Added empty Home screen 🧩 Added screen util to weather app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'core/routing/app_router.dart';
 import 'weather_app.dart';
 
 void main() {
-  runApp(const WeatherApp());
+  runApp(
+    WeatherApp(
+      appRouter: AppRouter(),
+    ),
+  );
 }
--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:weather_app/core/routing/app_router.dart';
+
+import 'core/routing/routes.dart';
 
 class WeatherApp extends StatelessWidget {
-  const WeatherApp({super.key});
+  final AppRouter appRouter;
+  const WeatherApp({
+    super.key,
+    required this.appRouter,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter.onGenerateRoute,
+        initialRoute: Routes.home,
+      ),
     );
   }
 }
