c37730bfc9b28a6faeb3ae4d03e890722aa640f3
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-08-22 20:03:02 +0300

    📏 Added spacing helper 🧩 Added extinsions to context, string, & list 🎨 Added colors manager

---
+++ lib/core/theming/colors_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color blackColor = Color(0xff181D1F);
+  static const Color whiteColor = Color(0xffF3F3F3);
+  static const Color blueColor = Color(0xff21394E);
+  static const Color greyColor = Color(0xff202329);
+}
