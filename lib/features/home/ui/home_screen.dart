edc7e7fca8591e3caeeda094af9beaefa302d43e
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-08-25 08:45:48 +0300

    🗺️ Added flutter_map & latlong2 packages 📍 Added MyLocationOnMap widget

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../core/helpers/spacing.dart';
 import 'widgets/hourly_forcast_list_view.dart';
+import 'widgets/my_location_on_map.dart';
 import 'widgets/tempratutre_and_icon.dart';
 import 'widgets/home_screen.dart';
 import 'widgets/weather_days_selection.dart';
@@ -17,18 +18,22 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           width: double.infinity,
           margin: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-          child: Column(
-            children: [
-              const HomeTopBar(),
-              verticalSpacing(20),
-              const TempratutreAndIcon(),
-              verticalSpacing(20),
-              const WeatherDetails(),
-              verticalSpacing(20),
-              const WeatherDaysSelection(),
-              verticalSpacing(10),
-              const HourlyForcastListView(),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const HomeTopBar(),
+                verticalSpacing(20),
+                const TempratutreAndIcon(),
+                verticalSpacing(20),
+                const WeatherDetails(),
+                verticalSpacing(20),
+                const WeatherDaysSelection(),
+                verticalSpacing(10),
+                const HourlyForcastListView(),
+                verticalSpacing(15),
+                const MyLocationOnMap(),
+              ],
+            ),
           ),
         ),
       ),
