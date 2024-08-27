f6ae97ec84c79ffd68e4b85425a1db8ccc848d84
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-08-27 05:31:23 +0300

    📜 Added Api constants file 🌐 Added Api service file

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = 'https://api.openweathermap.org/data/2.5/';
+  static const String weatherEp = 'weather';
+  static const String apiKey = '7dd6c79f3fb752423c097cb7039fcf4b';
+}
---
+++ lib/core/networking/api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import '../../features/home/data/models/current_weather_data_response.dart';
+import '../../features/home/data/models/current_weather_data_request.dart';
+import 'api_constants.dart';
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @GET(ApiConstants.weatherEp)
+  Future<CurrentWeatherDataResponse> getWeatherData(
+    @Queries() CurrentWeatherDataRequest request,
+  );
+}
