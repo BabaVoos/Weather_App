cf0af7edbb3f27453b4b44f620c311d522cbc08c
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-09-02 16:12:11 +0300

    ✨ Implemenedt filtering functions in HomeCubit 🛠️ Added new function to DateTime extension 🎨 Updated UI for selected item in forecast data ♻️ Refactored code to adapt to changes

--- lib/features/home/logic/cubit/home_cubit.dart
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
+import '../../data/models/five_days_forecast_response.dart';
 import '../../data/repos/five_days_forecast_data_repo.dart';
 import '../../../../core/helpers/location_helper.dart';
 import '../../data/repos/current_weather_data_repo.dart';
@@ -18,16 +19,18 @@ class HomeCubit extends Cubit<HomeState> {
 
   String? cityName;
   Position? currentLocation;
+  List<FivdeDaysForecastListItem> forecastData = [];
 
-  /// Asynchronously fetches the current weather and a 5-day forecast.
   Future<void> getWeatherData() async {
-    await getCurrentWeatherData();
-    await getFiveDaysForecastData();
+    await _fetchCurrentWeather();
+    await _fetchFiveDaysForecast();
   }
 
-  Future<void> getCurrentWeatherData() async {
+  Future<void> _fetchCurrentWeather() async {
     emit(const HomeState.currentWeatherDataloading());
-    await getCurrentLocationAndCity();
+
+    await _fetchLocationAndCity();
+
     final result = await _currentWeatherDataRepo.getCurrentWeatherData(
       CurrentWeatherDataRequest(
         lat: currentLocation!.latitude,
@@ -35,13 +38,12 @@ class HomeCubit extends Cubit<HomeState> {
         appId: ApiConstants.apiKey,
       ),
     );
+
     result.when(
-      success: (currentWeatherDataResponse) {
-        emit(
-          HomeState.currentWeatherDataSuccess(
-            currentWeatherDataResponse: currentWeatherDataResponse,
-          ),
-        );
+      success: (response) {
+        emit(HomeState.currentWeatherDataSuccess(
+          currentWeatherDataResponse: response,
+        ));
       },
       failure: (failure) {
         emit(HomeState.currentWeatherDataError(failure));
@@ -49,13 +51,14 @@ class HomeCubit extends Cubit<HomeState> {
     );
   }
 
-  Future<void> getCurrentLocationAndCity() async {
+  Future<void> _fetchLocationAndCity() async {
     currentLocation = await LocationHelper.getCurrentLocation();
     cityName = await LocationHelper.getCurrentCity(currentLocation);
   }
 
-  Future<void> getFiveDaysForecastData() async {
+  Future<void> _fetchFiveDaysForecast() async {
     emit(const HomeState.fiveDaysForecastDataLoading());
+
     final result = await _fiveDaysForecastDataRepo.getFiveDaysForecastData(
       CurrentWeatherDataRequest(
         lat: currentLocation!.latitude,
@@ -63,17 +66,61 @@ class HomeCubit extends Cubit<HomeState> {
         appId: ApiConstants.apiKey,
       ),
     );
+
     result.when(
-      success: (fivdeDaysForecastList) {
-        emit(
-          HomeState.fiveDaysForecastDataSuccess(
-            fiveDaysForecastList: fivdeDaysForecastList,
-          ),
-        );
+      success: (response) async {
+        forecastData = response;
+        final filteredList = await _filterForecastDataForDay(0);
+        emit(HomeState.fiveDaysForecastDataSuccess(
+          fiveDaysForecastList: filteredList,
+        ));
       },
       failure: (failure) {
         emit(HomeState.fiveDaysForecastDataError(failure));
       },
     );
+  }
+
+  Future<List<FivdeDaysForecastListItem>> _filterForecastDataForDay(
+    int index,
+  ) async {
+    final filteredForecastData = await _groupForecastDataByDate();
+    return filteredForecastData.isNotEmpty ? filteredForecastData[index] : [];
+  }
+
+  Future<List<List<FivdeDaysForecastListItem>>>
+      _groupForecastDataByDate() async {
+    if (forecastData.isEmpty) return [];
+
+    final groupedData = <DateTime, List<FivdeDaysForecastListItem>>{};
+
+    for (var data in forecastData) {
+      final dateTime = DateTime.parse(data.date!);
+      final date = DateTime(dateTime.year, dateTime.month, dateTime.day);
+      final formattedTime = _formatTime(dateTime);
+
+      groupedData.putIfAbsent(date, () => []).add(
+            FivdeDaysForecastListItem(
+              date: formattedTime,
+              main: data.main,
+            ),
+          );
+    }
+
+    return groupedData.values.toList();
+  }
+
+  String _formatTime(DateTime dateTime) {
+    final hour = dateTime.hour;
+    final hourFormatted = hour == 0 ? 12 : (hour > 12 ? hour - 12 : hour);
+    final amPm = hour < 12 ? 'am' : 'pm';
+    return '$hourFormatted $amPm';
+  }
+
+  void updateIndex(int index) async {
+    final filteredList = await _filterForecastDataForDay(index);
+    emit(HomeState.fiveDaysForecastDataSuccess(
+      fiveDaysForecastList: filteredList,
+    ));
   }
 }
--- lib/features/home/ui/widgets/hourly_forcast_item.dart
+++ lib/features/home/ui/widgets/hourly_forcast_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../data/models/five_days_forecast_response.dart';
 import '../../../../core/helpers/spacing.dart';
 import '../../../../core/theming/colors_manager.dart';
 import '../../../../core/theming/text_styles.dart';
@@ -8,11 +9,11 @@ import '../../../../core/helpers/extinsions.dart';
 class HourlyForcastItem extends StatelessWidget {
   const HourlyForcastItem({
     super.key,
-    required this.temp,
+    required this.fivdeDaysForecastListItem,
     required this.index,
   });
 
-  final double temp;
+  final FivdeDaysForecastListItem fivdeDaysForecastListItem;
   final int index;
 
   @override
@@ -23,23 +24,24 @@ class HourlyForcastItem extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(16.r)),
         color: ColorsManager.greyColor,
       ),
-      child: temp == 0
+      child: fivdeDaysForecastListItem.main?.temp == 0
           ? const Center(child: CircularProgressIndicator())
           : Column(
               children: [
                 Text(
-                  getHour(index),
+                  fivdeDaysForecastListItem.date ?? '',
                   style: TextStylesManager.font16GreyRegular,
                 ),
                 verticalSpacing(5),
                 Text(
-                  getWeatherIcon(temp.toCelsius()),
+                  _getWeatherIcon(
+                      fivdeDaysForecastListItem.main?.temp?.toCelsius()),
                   style: TextStylesManager.font20WhiteRegular
                       .copyWith(fontSize: 30.sp),
                 ),
                 verticalSpacing(5),
                 Text(
-                  '${temp.toCelsius()}°',
+                  '${fivdeDaysForecastListItem.main?.temp?.toCelsius()}°',
                   style: TextStylesManager.font16WhiteRegular,
                 ),
               ],
@@ -47,32 +49,8 @@ class HourlyForcastItem extends StatelessWidget {
     );
   }
 
-  /// Returns the hour based on the index
-  String getHour(int index) {
-    switch (index) {
-      case 0:
-        return '12 am';
-      case 1:
-        return '3 am';
-      case 2:
-        return '6 am';
-      case 3:
-        return '9 am';
-      case 4:
-        return '12 pm';
-      case 5:
-        return '3 pm';
-      case 6:
-        return '6 pm';
-      case 7:
-        return '9 pm';
-      default:
-        return '12 pm';
-    }
-  }
-
   /// Returns the weather icon based on the temperature
-  String getWeatherIcon(int temperature) {
+  String _getWeatherIcon(int? temperature) {
     const weatherIcons = [
       {'max': 0, 'icon': '❄️'},
       {'max': 10, 'icon': '🌬️'},
@@ -83,7 +61,7 @@ class HourlyForcastItem extends StatelessWidget {
 
     for (var entry in weatherIcons) {
       int maxTemp = entry['max'] as int;
-      if (temperature <= maxTemp) {
+      if (temperature! <= maxTemp) {
         return entry['icon'] as String;
       }
     }
