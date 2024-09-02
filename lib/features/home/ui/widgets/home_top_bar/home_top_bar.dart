30058f2d5e32aad472870c18eac6ccf6917e9992
BabaVoos <91697196+BabaVoos@users.noreply.github.com>
2024-09-02 19:39:01 +0300

    Removed actions button from home top bar

--- lib/features/home/ui/widgets/home_top_bar/home_top_bar.dart
+++ lib/features/home/ui/widgets/home_top_bar/home_top_bar.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../logic/cubit/home_state.dart';
 import '../../../../../core/helpers/extinsions.dart';
-import '../../../../../core/theming/colors_manager.dart';
 import '../../../../../core/theming/text_styles.dart';
 import 'city_name.dart';
 
@@ -27,23 +25,7 @@ class HomeTopBar extends StatelessWidget {
             buildDate(),
           ],
         ),
-        const Spacer(),
-        buildActionButton(),
       ],
-    );
-  }
-
-  Container buildActionButton() {
-    return Container(
-      padding: EdgeInsets.all(10.r),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8.r)),
-        color: ColorsManager.greyColor,
-      ),
-      child: const Icon(
-        Icons.light_mode_outlined,
-        color: ColorsManager.whiteColor,
-      ),
     );
   }
 
